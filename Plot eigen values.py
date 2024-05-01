4354fd8493d34d0d68ffb608e98f512ea5c9c4b3
o-k20 <168195755+o-k20@users.noreply.github.com>
2024-05-01 14:55:48 +0200

    Plot_eigen values.py

--- Plot eigen values.py
+++ Plot eigen values.py
@@ -2,19 +2,19 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # Create plots
-fig = plt.figure(figsize=(14, 7))
+fig = plt.figure(figsize=(15, 5))
 ax1 = fig.add_subplot(121, projection='3d')
 ax2 = fig.add_subplot(122, projection='3d')
 
 # Eigenvalue 1
 ax1.plot_surface(X, Y, np.abs(eigenvalues[0]), cmap='viridis')
-ax1.set_title('Absolute Value of First Eigenvalue')
+ax1.set_title('Eigenvalue 1')
 ax1.set_xlabel('x')
 ax1.set_ylabel('y')
 
 # Eigenvalue 2
 ax2.plot_surface(X, Y, np.abs(eigenvalues[1]), cmap='viridis')
-ax2.set_title('Absolute Value of Second Eigenvalue')
+ax2.set_title('Eigenvalue 2')
 ax2.set_xlabel('x')
 ax2.set_ylabel('y')
 
